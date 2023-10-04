311c22d5914ce6f35900c41b985b15466a9f75c9
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-10-04 15:01:43 +0600

    _07_laravel multi auth with breeze part2

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,6 +29,17 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
+
+        $url = '';
+        if($request->user()->role === 'admin'){
+            $url = 'admin/dashboard';
+        }elseif($request->user()->role === 'agent'){
+            $url = 'agent/dashboard';
+        }elseif($request->user()->role === 'user'){
+            $url = '/dashboard';
+        }
+
+
         return redirect()->intended(RouteServiceProvider::HOME);
     }
 
