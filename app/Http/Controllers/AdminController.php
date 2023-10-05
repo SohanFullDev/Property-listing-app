dda7c584f78c68fef289dca350abb4f6d30c7ee0
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-10-05 15:35:31 +0600

    _19_admin profile & image update part3

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -40,5 +40,27 @@ class AdminController extends Controller
 
     }//end method
 
+    public function AdminProfileStore(Request $request){
+
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->username = $request->username;
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+
+        if($request->file('photo')){
+            $file = $request->file('photo');
+            $filename = date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'),$filename);
+            $data['photo'] = $filename;
+        }
+
+        $data->save();
+        return redirect()->back();
+
+    }// end method
+
 
 }
--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.admin_dashboard')
 @section('admin')
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 <div class="page-content">
 
 
@@ -58,29 +58,39 @@
              <div class="card">
               <div class="card-body">
 
-								<h6 class="card-title">Basic Form</h6>
+								<h6 class="card-title">Update Admin Profiel</h6>
 
-								<form class="forms-sample">
+			<form method="POST" action="{{ route('admin.profile.store') }}" class="forms-sample" enctype="multipart/form-data">
 									<div class="mb-3">
 										<label for="exampleInputUsername1" class="form-label">Username</label>
-										<input type="text" class="form-control" id="exampleInputUsername1" autocomplete="off" placeholder="Username">
+		<input type="text" name="username" class="form-control" id="exampleInputUsername1" autocomplete="off" value="{{ $profileData->username }}" placeholder="Username">
 									</div>
 									<div class="mb-3">
 										<label for="exampleInputEmail1" class="form-label">Email address</label>
-										<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+				<input type="email" name="email" class="form-control" autocomplete="off" id="exampleInputEmail1" value="{{ $profileData->email }}" placeholder="Email">
 									</div>
 									<div class="mb-3">
-										<label for="exampleInputPassword1" class="form-label">Password</label>
-										<input type="password" class="form-control" id="exampleInputPassword1" autocomplete="off" placeholder="Password">
+                                        <label for="exampleInputEmail1" class="form-label">Phone</label>
+            <input type="text" name="phone" class="form-control" autocomplete="off" id="exampleInputEmail1" value="{{ $profileData->phone }}" placeholder="Phone">
 									</div>
-									<div class="form-check mb-3">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-										<label class="form-check-label" for="exampleCheck1">
-											Remember me
-										</label>
+
+                                    <div class="mb-3">
+										<label for="exampleInputEmail1" class="form-label">Address</label>
+		<input type="text" name="address" class="form-control" autocomplete="off" id="exampleInputEmail1" value="{{ $profileData->address }}" placeholder="Address">
 									</div>
-									<button type="submit" class="btn btn-primary me-2">Submit</button>
-									<button class="btn btn-secondary">Cancel</button>
+
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Photo   </label>
+            <input class="form-control"  name="photo" type="file" id="image">
+        </div>
+
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">    </label>
+               <img id="showImage" class="wd-80 rounded-circle" src="{{ (!empty($profileData->photo)) ? url('upload/admin_images/'.$profileData->photo) : url('upload/no_image.jpg') }}" alt="profile">
+       </div>
+
+			<button type="submit" class="btn btn-primary me-2">Save Changes</button>
+
 								</form>
 
               </div>
@@ -101,7 +111,17 @@
 
 
 
-
+            <script type="text/javascript">
+                $(document).ready(function(){
+                    $('#image').change(function(e){
+                        var reader = new FileReader();
+                        reader.onload = function(e){
+                            $('#showImage').attr('src',e.target.result);
+                        }
+                        reader.readAsDataURL(e.target.files['0']);
+                    });
+                });
+            </script>
 
 
 
